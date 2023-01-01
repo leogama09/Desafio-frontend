b0f6d2bdc5fdb8ac4124b8086e89da35659f8aa6
Leonardo Gama <leogama09@gmail.com>
2023-01-01 18:05:09 -0300

    Update index.js

--- frontend/challenge/src/components/2-Section/index.js
+++ frontend/challenge/src/components/2-Section/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import hero from '../../assets/Hero.png'
+import heromobile from '../../assets/Hero mobile.png'
 import icono1 from '../../assets/icono 1.png'
 import icono2 from '../../assets/icono 2.png'
 import infos from '../../assets/buttons/mas infos.svg'
@@ -12,7 +13,8 @@ class Section extends Component {
             <>
             <section>
                 <div class="mireia">
-                    <img src={hero} alt="" />
+                    <img src={hero} alt="" class="hero" />
+                    <img src={heromobile} alt="" class="heromobile" />
                  </div>
 
                 <div class="information">
